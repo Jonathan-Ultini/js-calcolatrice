ce37bf7723eb879c8e174c310dba9bf6c88ca914
Jonathan-Ultini <jonathanultini@gmail.com>
2024-08-16 15:49:26 +0200

    funzione calcolatrice js

---
+++ calcolatrice.js
@@ -0,0 +1,31 @@
+class Calcolatrice {
+  constructor() {
+    // Associa l'elemento HTML con id "display" all'attributo displayElement
+    // Questo elemento sarà utilizzato per mostrare i numeri e i risultati della calcolatrice
+    this.displayElement = document.getElementById('display');
+
+    // Variabili per memorizzare l'ultimo operando inserito, l'ultima operazione eseguita
+    // e il risultato corrente
+    this.lastOperand = '';
+    this.lastOperation = undefined;
+    this.result = '';
+
+    // Ottiene gli stili CSS calcolati dell'elemento displayElement
+    const computedDisplayElement = window.getComputedStyle(this.displayElement, null);
+
+    // Memorizza la dimensione del font dell'elemento displayElement in un numero intero
+    this.defaultDisplayElementFontSize = parseInt(computedDisplayElement.getPropertyValue('font-size'));
+
+    // Calcola il numero massimo di caratteri che possono essere visualizzati nel displayElement
+    // senza che il testo trabocchi. Questo calcolo è basato sulla larghezza dell'elemento,
+    // il padding interno e una stima della larghezza di un carattere.
+    this.defaultDisplayElementMaxChars = Math.ceil(
+      (parseInt(computedDisplayElement.getPropertyValue('width'))  // Larghezza dell'elemento
+        - parseInt(computedDisplayElement.getPropertyValue('padding')) * 2)  // Sottrae il padding
+      / (this.defaultDisplayElementFontSize * 0.6)  // Stima della larghezza di un carattere
+    );
+  }
+}
+
+// Crea una nuova istanza della classe Calcolatrice e la memorizza nella variabile calcolatrice
+const calcolatrice = new Calcolatrice();
